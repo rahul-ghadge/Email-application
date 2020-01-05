581e2fe89387489afc933aab92563dc7291cc953
rahul_ghadage <rahul.g5557@gmail.com>
2020-01-05 18:11:32 +0530

    Added compose mail feature

--- email-app/src/app/email/email.component.ts
+++ email-app/src/app/email/email.component.ts
@@ -11,6 +11,9 @@ export class EmailComponent implements OnInit {
 
   emailData: any = [];
   folder: string = 'INBOX';
+  compose = false;
+  composeMailData: any = {};
+  editable = true;
 
   constructor(private service: EmailService) { }
 
@@ -32,22 +35,26 @@ export class EmailComponent implements OnInit {
   changeFolder(folderName: string) {
     console.log(folderName);
     this.folder = folderName;
+    this.compose = false;
+    this.composeMailData = {};
+    
 
     this.service.loadInbox("", this.folder)
     .subscribe(resp => {
 
       console.log(resp);
-      let tempMails = [];
+      //let tempMails = [];
 
       if (resp.statusCode == 200) {
-        //this.emailData = resp.model;
-        this.emailData.forEach(ele => {
-          if(this.folder != ele.folder) {
-            tempMails.push(ele);
-          }
-      });
-      console.log(this.emailData + 'temp mails : ' + tempMails);
-      this.emailData = [...tempMails];
+        this.emailData = resp.model;
+      //   this.emailData.forEach(ele => {
+      //     if(this.folder != ele.folder) {
+      //       tempMails.push(ele);
+      //     }
+      // }
+      //);
+      //console.log(this.emailData + 'temp mails : ' + tempMails);
+      //this.emailData = [...tempMails];
       } else {
         alert(resp.erroMessage);
       }
@@ -55,12 +62,78 @@ export class EmailComponent implements OnInit {
   }
 
   moveToFolder(folderName, id) {
+    this.compose = false;
+
     this.service.moveToFolder(folderName, id).subscribe(resp => {
 
       console.log(resp);
 
       if (resp.statusCode == 200) {
         this.emailData = resp.model;
+      } else {
+        alert(resp.erroMessage);
+      }
+    });
+  }
+
+  
+  composeMail() {
+    this.compose = true;
+    this.editable = true;
+    this.composeMailData = {};
+  }
+
+  viewEmail(emailData: any) {
+    this.editable = false;
+    this.composeMailData = {...emailData};
+    this.compose = true;
+  }
+
+  sendEmail() {
+
+    this.composeMailData.folder = "SEND";
+    this.composeMailData.isSpam = false;
+
+    let tempArr = [];
+    if(this.composeMailData.receipt.indexOf(";") != -1) {     
+      this.composeMailData.receipt.split(";").forEach(element => {
+        tempArr.push(element);
+      });     
+    } else {
+      tempArr.push(this.composeMailData.receipt);
+    }
+    this.composeMailData.receipt = tempArr;
+    
+   
+    if(this.composeMailData.ccReceipt) {
+      tempArr = [];
+      if(this.composeMailData.ccReceipt.indexOf(";") != -1) {     
+        this.composeMailData.ccReceipt.split(";").forEach(element => {
+          tempArr.push(element);
+        });        
+      } else {
+        tempArr.push(this.composeMailData.receipt);
+      }
+
+      this.composeMailData.ccReceipt = tempArr;
+    }
+
+
+    if(!this.composeMailData.sender)
+        this.composeMailData.sender = "defaultsender@test.com"
+    
+    console.log("Email sender : " + this.composeMailData.sender);
+    console.log("Email receipt : " + this.composeMailData.receipt);
+    console.log("Email ccReceipt : " + this.composeMailData.ccReceipt);
+    console.log("Email subject : " + this.composeMailData.subject);
+    console.log("Email folder : " + this.composeMailData.folder);
+
+    this.service.sendEmail(this.composeMailData).subscribe(resp => {
+
+      console.log(resp);
+
+      if (resp.statusCode == 200) {
+        this.changeFolder(this.composeMailData.folder);
       } else {
         alert(resp.erroMessage);
       }
--- email-app/src/app/email/email.service.ts
+++ email-app/src/app/email/email.service.ts
@@ -1,22 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  API_ENDPOINT = 'http://localhost:8080/email';
+  //HOST = 'http://localhost:8080';
+  HOST = "http://192.168.0.104:8080"
+  API_ENDPOINT = this.HOST + '/email';
 
   constructor(private http : HttpClient) { }
 
+ 
+
 
   loadInbox(sender, folder):any {
-    return this.http.get<any>(this.API_ENDPOINT + "?folder=" + folder + '&sender=' + sender);
+    return this.http.get<any>(this.API_ENDPOINT + "/get?folder=" + folder + '&sender=' + sender);
   }
 
   moveToFolder(folder, id):any {
-    return this.http.get<any>(this.API_ENDPOINT + "/move" + "?folder=" + folder + '&id=' + id);
+    return this.http.get<any>(this.API_ENDPOINT + "/move?folder=" + folder + '&id=' + id);
+  }
+
+  sendEmail(emailData) {
+
+    //httpOptions.headers.append('Access-Control-Allow-Origin', this.HOST);
+    httpOptions.headers.append('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS, DELETE, PATCH');
+    httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With, content-type Origin, Accept');
+    httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
+    httpOptions.headers.append("Access-Control-Allow-Origin", "*");
+
+
+    return this.http.post<any>(this.API_ENDPOINT + "/send-email", emailData, httpOptions);
   }
 
 }
